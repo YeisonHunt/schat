387bed9aac0a834a464de2926b58f14203bd3a74
Yeison Caicedo <yecaicedo@unicauca.edu.co>
2020-01-30 17:01:39 -0500

    asd

---
+++ test.js
@@ -0,0 +1,40 @@
+<script src="https://app.five9.com/consoles/SocialWidget/five9-social-widget.min.js"></script>
+	<script>
+		var options = {
+			"rootUrl": "https://app.five9.com/consoles/",
+"type": "chat",
+"title": "asdasdasd",
+"tenant": "Enter Domain asdasdName",
+"profiles": "asdasdasd",
+"showProfiles": true,
+"theme": "high-contrast.css",
+	"surveyOptions": {
+			"showComment": true,
+"requireComment": false
+},
+	"fields": {
+			"name": {
+			"value": "",
+"show": true,
+"label": "Name"
+},
+		"email": {
+			"value": "",
+"show": true,
+"label": "Email"
+}
+},
+"playSoundOnMessage": true,
+"allowCustomerToControlSoundPlay": false,
+"lang": "https://five9-chat-console.herokuapp.com/lang-custom.js",
+"showEmailButton": true,
+"hideDuringAfterHours": false,
+"useBusinessHours": false,
+"showPrintButton": false,
+"allowUsabilityMenu": false
+};
+
+
+Five9SocialWidget.addWidget(options);
+
+</script>
